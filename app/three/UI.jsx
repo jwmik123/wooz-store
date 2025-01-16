92757d3e7df40672d8629dfeb3bbb7ceb337d9b7
Joël Mik <contact@joelmik.nl>
2025-01-16 20:49:21 +0100

    heh

--- app/three/UI.jsx
+++ app/three/UI.jsx
@@ -1,5 +1,4 @@
 import { atom, useAtom } from "jotai";
-import { useEffect } from "react";
 
 const pictures = [
   "wz04",
@@ -36,50 +35,5 @@ pages.push({
 });
 
 export const UI = () => {
-  const [page, setPage] = useAtom(pageAtom);
-
-  useEffect(() => {
-    const audio = new Audio("/assets/sounds/page-flip-01a.mp3");
-    audio.play();
-  }, [page]);
-
-  return (
-    <>
-      <main className="fixed inset-0 z-10 flex flex-col justify-between pointer-events-none select-none ">
-        <a
-          className="mt-10 ml-10 pointer-events-auto"
-          href="https://lessons.wawasensei.dev/courses/react-three-fiber"
-        >
-          <img className="w-20" src="/images/wawasensei-white.png" />
-        </a>
-        <div className="flex justify-center w-full overflow-auto pointer-events-auto">
-          <div className="flex items-center max-w-full gap-4 p-10 overflow-auto">
-            {[...pages].map((_, index) => (
-              <button
-                key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                  index === page
-                    ? "bg-white/90 text-black"
-                    : "bg-black/30 text-white"
-                }`}
-                onClick={() => setPage(index)}
-              >
-                {index === 0 ? "Cover" : `Page ${index}`}
-              </button>
-            ))}
-            <button
-              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                page === pages.length
-                  ? "bg-white/90 text-black"
-                  : "bg-black/30 text-white"
-              }`}
-              onClick={() => setPage(pages.length)}
-            >
-              Back Cover
-            </button>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+  return null;
 };
